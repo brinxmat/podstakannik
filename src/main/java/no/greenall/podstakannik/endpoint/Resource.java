31c3e6182eb728d9a19fad8ed4136fe82552eb20
Rurik Greenall <rurik.greenall@gmail.com>
2017-02-14 23:48:44 +0100

    Add base resource stub

--- src/main/java/no/greenall/podstakannik/endpoint/Resource.java
+++ src/main/java/no/greenall/podstakannik/endpoint/Resource.java
@@ -1,7 +1,20 @@
 package no.greenall.podstakannik.endpoint;
 
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
 /**
  * Responsibility: Provide resources for application.
  */
-public class Resource {
+@Singleton
+@Path("/")
+public final class Resource {
+    @GET
+    @Produces("text/plain")
+    public Response base(){
+        return Response.ok().build();
+    }
 }
--- src/test/java/no/greenall/podstakannik/endpoint/ResourceTest.java
+++ src/test/java/no/greenall/podstakannik/endpoint/ResourceTest.java
@@ -1,15 +1,27 @@
 package no.greenall.podstakannik.endpoint;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
  * Responsibility: Test Resource.class.
  */
 public class ResourceTest {
+
     @Test
     public void it_exists() {
         assertNotNull(new Resource());
+    }
+
+    @Test
+    public void test_base() {
+        Resource resource = new Resource();
+        assertEquals(Response.Status.OK.getStatusCode(), resource.base().getStatus());
     }
 }
